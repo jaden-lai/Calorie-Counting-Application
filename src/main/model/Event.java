bf51ad0506bcfb25ba2ac496ede97e20d400650a
jlai2 <jlai14@student.ubc.ca>
2023-03-26 23:55:07 -0700

    Phase 4: Task 2

--- src/main/model/Event.java
+++ src/main/model/Event.java
@@ -3,7 +3,6 @@ package model;
 import java.util.Calendar;
 import java.util.Date;
 
-
 /**
  * Represents an alarm system event.
  */
@@ -15,8 +14,7 @@ public class Event {
     /**
      * Creates an event with the given description
      * and the current date/time stamp.
-     *
-     * @param description a description of the event
+     * @param description  a description of the event
      */
     public Event(String description) {
         dateLogged = Calendar.getInstance().getTime();
@@ -25,8 +23,7 @@ public class Event {
 
     /**
      * Gets the date of this event (includes time).
-     *
-     * @return the date of the event
+     * @return  the date of the event
      */
     public Date getDate() {
         return dateLogged;
@@ -34,26 +31,24 @@ public class Event {
 
     /**
      * Gets the description of this event.
-     *
-     * @return the description of the event
+     * @return  the description of the event
      */
-    public String getDescription() {
+    public String getDescription () {
         return description;
     }
 
     @Override
     public boolean equals(Object other) {
-        if (other == null) {
+        if (other == null)
             return false;
-        }
-        if (other.getClass() != this.getClass()) {
+
+        if (other.getClass() != this.getClass())
             return false;
-        }
 
         Event otherEvent = (Event) other;
 
-        return (this.dateLogged.equals(otherEvent.dateLogged)
-                && this.description.equals(otherEvent.description));
+        return (this.dateLogged.equals(otherEvent.dateLogged) &&
+                this.description.equals(otherEvent.description));
     }
 
     @Override
